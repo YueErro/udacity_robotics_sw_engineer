aa132601ea3a8455125bdea7f177834084d1edf8
YueErro <yue.trbj@gmail.com>
2020-10-10 12:06:08 +0200

    add process_image node and chase functionality

--- 3-ros_essentials/catkin_ws/src/ball_chaser/README.md
+++ 3-ros_essentials/catkin_ws/src/ball_chaser/README.md
@@ -10,13 +10,15 @@ roslaunch my_robot world.launch rviz:=false
 
 ![](images/my_robot.png)
 
-In a new terminal run the `drive_bot` node:
+## Drive bot
+
+In a second terminal run the `drive_bot` node:
 ```sh
 source ~/udacity_robotics_sw_engineer/3-ros_essentials/catkin_ws/devel/setup.bash
 rosrun ball_chaser drive_bot
 ```
 
-In a new different terminal request a `ball_chaser/command_robot` service:
+In a third terminal request a `ball_chaser/command_robot` service:
 ```sh
 source ~/udacity_robotics_sw_engineer/3-ros_essentials/catkin_ws/devel/setup.bash
 # Drive your robot forward
@@ -32,3 +34,18 @@ angular_z: -0.5"
 rosservice call /ball_chaser/command_robot "linear_x: 0.0
 angular_z: 0.0"
 ```
+
+## Process_image
+In a second terminal run the `drive_bot` and the `process_image` node:
+```sh
+source ~/udacity_robotics_sw_engineer/3-ros_essentials/catkin_ws/devel/setup.bash
+roslaunch ball_chaser ball_chaser.launch
+```
+
+In a third terminal visualize the robot's camera images:
+```sh
+source ~/udacity_robotics_sw_engineer/3-ros_essentials/catkin_ws/devel/setup.bash
+rosrun rqt_image_view rqt_image_view
+```
+
+Place the white ball at different positions in front of the robot and see if the robot is capable of chasing the ball!
---
+++ 3-ros_essentials/catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -0,0 +1,111 @@
+#include <ros/ros.h>
+#include "ball_chaser/DriveToTarget.h"
+#include <sensor_msgs/Image.h>
+
+#include <vector>
+#include <algorithm>
+
+// Define a global client that can request services
+ros::ServiceClient client;
+
+// This function calls the command_robot service to drive the robot in the specified
+// direction
+void drive_robot(float lin_x, float ang_z)
+{
+  // Request a service and pass the velocities to it to drive the robot
+  ball_chaser::DriveToTarget dtt_srv;
+  dtt_srv.request.linear_x = lin_x;
+  dtt_srv.request.angular_z = ang_z;
+
+  ROS_INFO_STREAM("Commanding the robot linear: " << lin_x << ", angular: " << ang_z);
+
+  if (!client.call(dtt_srv))
+  {
+    ROS_ERROR_STREAM("Failed to call service command_robot");
+  }
+}
+
+// This callback function continuously executes and reads the image data
+void process_image_callback(const sensor_msgs::Image img)
+{
+  int white_pixel = 255;
+  unsigned int ball_pos;
+  // {left, right, middle}
+  std::vector<int> ball_pixels = { 0, 0, 0 };
+
+  // Loop through each pixel in the image and check if there's a bright white one
+  // Iterate threes due to three possible locations
+  for (unsigned int i = 0; i < img.height * img.step; i++)
+  {
+    if (img.data[i] == white_pixel)
+    {
+      ROS_INFO_STREAM("The ball is in the image");
+      ball_pos = i % img.step;
+      // Then, identify if this pixel falls in the left, mid, or right side of the image
+      if (ball_pos < img.step / 3)
+      {
+        ROS_INFO_STREAM("Ball pixel on the left hand side");
+        ball_pixels[0] += 1;
+      }
+      else if (ball_pos > img.step / 3)
+      {
+        ROS_INFO_STREAM("Ball pixel on the right hand side");
+        ball_pixels[1] += 1;
+      }
+      else
+      {
+        ROS_INFO_STREAM("Ball pixel in the middle");
+        ball_pixels[2] += 1;
+      }
+      break;
+    }
+  }
+  // Depending on the white ball position, call the drive_bot function and pass
+  // velocities
+  // to it
+  if ((ball_pixels[0] | ball_pixels[1] | ball_pixels[2]) != 0)
+  {
+    int location =
+        std::max_element(ball_pixels.begin(), ball_pixels.end()) - ball_pixels.begin();
+    float closeness_indicator = *std::max_element(ball_pixels.begin(), ball_pixels.end());
+    if (location == 0)
+    {
+      ROS_INFO_STREAM("Most part of the ball is on the left hand side");
+      drive_robot(0.1f / closeness_indicator, 0.1f * closeness_indicator);
+    }
+    else if (location == 1)
+    {
+      ROS_INFO_STREAM("Most part of the ball is on the right hand side");
+      drive_robot(0.1f / closeness_indicator, -0.1f * closeness_indicator);
+    }
+    else
+    {
+      ROS_INFO_STREAM("Most part of the ball is in the middle");
+      drive_robot(0.1f, 0.0f);
+    }
+  }
+  // Request a stop when there's no white ball seen by the camera
+  else
+  {
+    drive_robot(0.0f, 0.0f);
+  }
+}
+
+int main(int argc, char** argv)
+{
+  // Initialize the process_image node and create a handle to it
+  ros::init(argc, argv, "process_image");
+  ros::NodeHandle n;
+
+  // Define a client service capable of requesting services from command_robot
+  client = n.serviceClient<ball_chaser::DriveToTarget>("/ball_chaser/command_robot");
+
+  // Subscribe to /camera/rgb/image_raw topic to read the image data inside the
+  // process_image_callback function
+  ros::Subscriber sub1 = n.subscribe("/camera/rgb/image_raw", 10, process_image_callback);
+
+  // Handle ROS communication events
+  ros::spin();
+
+  return 0;
+}
