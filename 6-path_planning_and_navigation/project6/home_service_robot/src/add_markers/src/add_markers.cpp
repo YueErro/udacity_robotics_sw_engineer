2fcecc0af444ff669f67f4b1b455e0a5e2b7032f
YueErro <yue.trbj@gmail.com>
2020-12-28 21:43:26 +0100

    feat: Add cube marker in RViz

---
+++ 6-path_planning_and_navigation/project6/home_service_robot/src/add_markers/src/add_markers.cpp
@@ -0,0 +1,79 @@
+#include <ros/ros.h>
+#include <visualization_msgs/Marker.h>
+
+int main( int argc, char** argv )
+{
+  ros::init(argc, argv, "add_markers");
+  ros::NodeHandle n;
+  ros::Rate r(1);
+  ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+
+  // Set our initial shape type to be a cube
+  uint32_t shape = visualization_msgs::Marker::CUBE;
+
+  visualization_msgs::Marker marker;
+  // Set the frame ID and timestamp.  See the TF tutorials for information on these.
+  marker.header.frame_id = "map";
+  marker.header.stamp = ros::Time::now();
+
+  // Set the namespace and id for this marker.  This serves to create a unique ID
+  // Any marker sent with the same namespace and id will overwrite the old one
+  marker.ns = "virtual_cube";
+  marker.id = 0;
+
+  // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
+  marker.type = shape;
+
+  // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+  marker.action = visualization_msgs::Marker::ADD;
+
+  // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+  marker.pose.position.x = -0.5;
+  marker.pose.position.y = 0.5;
+  marker.pose.position.z = 0;
+  marker.pose.orientation.x = 0.0;
+  marker.pose.orientation.y = 0.0;
+  marker.pose.orientation.z = 0.0;
+  marker.pose.orientation.w = 1.0;
+
+  // Set the scale of the marker -- 0.125x0.125x0.125 here means 0.125m on a side
+  marker.scale.x = 0.125;
+  marker.scale.y = 0.125;
+  marker.scale.z = 0.125;
+
+  // Set the color -- be sure to set alpha to something non-zero!
+  marker.color.r = 0.0f;
+  marker.color.g = 0.0f;
+  marker.color.b = 1.0f;
+  marker.color.a = 1.0;
+
+  marker.lifetime = ros::Duration();
+
+  // Publish the marker
+  while (marker_pub.getNumSubscribers() < 1)
+  {
+    if (!ros::ok())
+    {
+      return 0;
+    }
+    ROS_WARN_ONCE("Please create a subscriber to the marker");
+    sleep(1);
+  }
+  marker_pub.publish(marker);
+
+  r.sleep();
+  ros::Duration(5.0).sleep();
+
+  ROS_INFO_STREAM("Hidding the marker");
+  marker.action = visualization_msgs::Marker::DELETE;
+  marker_pub.publish(marker);
+  r.sleep();
+  ros::Duration(5.0).sleep();
+
+  ROS_INFO_STREAM("Publishing the marker at the drop off zone");
+  marker.pose.position.x = 0.5;
+  marker.pose.position.y = -0.5;
+  marker.action = visualization_msgs::Marker::ADD;
+  marker_pub.publish(marker);
+  r.sleep();
+}
