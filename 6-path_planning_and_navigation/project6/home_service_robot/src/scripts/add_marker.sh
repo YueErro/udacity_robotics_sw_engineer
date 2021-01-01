9164b28eec4027ced5d605ff8a6fc4e96289dd97
YueErro <yue.trbj@gmail.com>
2021-01-01 17:52:42 +0100

    docs: Add bash scripts

---
+++ 6-path_planning_and_navigation/project6/home_service_robot/src/scripts/add_marker.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+xterm  -e  " source $HOME/github/udacity_robotics_sw_engineer/6-path_planning_and_navigation/project6/home_service_robot/devel/setup.bash" &
+sleep 5
+xterm  -e  " export TURTLEBOT3_MODEL=burger && roslaunch turtlebot3_gazebo turtlebot3_world.launch" &
+sleep 5
+xterm  -e  " export TURTLEBOT3_MODEL=burger && roslaunch turtlebot3_navigation turtlebot3_navigation.launch" &
+sleep 5
+xterm  -e  " rosrun add_markers add_markers_node"
---
+++ 6-path_planning_and_navigation/project6/home_service_robot/src/scripts/test_navigation.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+xterm  -e  " source $HOME/github/udacity_robotics_sw_engineer/6-path_planning_and_navigation/project6/home_service_robot/devel/setup.bash" &
+sleep 5
+xterm  -e  " export TURTLEBOT3_MODEL=burger && roslaunch turtlebot3_gazebo turtlebot3_world.launch" &
+sleep 5
+xterm  -e  " export TURTLEBOT3_MODEL=burger && roslaunch turtlebot3_navigation turtlebot3_navigation.launch"
